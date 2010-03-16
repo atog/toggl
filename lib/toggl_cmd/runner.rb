c3b2293caa5120d28d42745f1d517ba0e167c8d7
Koen Van der Auwera <koen@atog.be>
2010-03-16 10:12:13 +0100

    toggl-gem name

--- lib/toggl_cmd/runner.rb
+++ lib/toggl_cmd/runner.rb
@@ -2,17 +2,19 @@ require "optparse"
 
 module TogglCmd
   
+  NAME = "toggl-gem"
+  
   class Runner
     
     def self.toggl(args)
       token = IO.readlines(File.expand_path("~/.toggl")).join.strip
       options = RunnerOptions.new(args)
       if options[:tasks]
-        puts Toggl.new(token, "toggl-gem").tasks
+        puts Toggl.new(token, NAME).tasks
       elsif options[:projects]        
-        puts Toggl.new(token, "toggl-gem").projects
+        puts Toggl.new(token, NAME).projects
       elsif options.any?        
-        Toggl.new(token, "toggl-gem", options.delete(:debug)).create_task(options)
+        Toggl.new(token, NAME, options.delete(:debug)).create_task(options)
       else
         puts options.opts
       end
