59b40fd9e784358e73553ef5a0fb8886c15c80d1
Koen Van der Auwera <koen@atog.be>
2010-06-23 21:35:35 +0200

    add task id in view

--- lib/toggl_cmd/runner.rb
+++ lib/toggl_cmd/runner.rb
@@ -2,36 +2,36 @@ require "optparse"
 require "chronic_duration"
 
 module TogglCmd
-  
+
   NAME = "toggl-gem"
   PROJECT_FIELDS = %w(client name)
-  TASK_FIELDS = %w(project description start duration billable)
-  
+  TASK_FIELDS = %w(id project description start duration billable)
+
   class Runner
-    
+
     def self.toggl(args)
       token = IO.readlines(File.expand_path("~/.toggl")).join.strip
       options = RunnerOptions.new(args)
       if options[:tasks]
         prettify_tasks(Toggl.new(token, NAME).tasks)
-      elsif options[:projects]        
+      elsif options[:projects]
         prettify_projects(Toggl.new(token, NAME).projects)
-      elsif options.any?        
+      elsif options.any?
         prettify_tasks(Toggl.new(token, NAME, options.delete(:debug)).create_task(options))
       else
         puts options.opts
       end
     end
-    
+
     private
-    
+
     def self.prettify_tasks(values)
       values = [values] unless values.is_a?(Array)
-      values.each do |value| 
+      values.each do |value|
         value["project"]    = value["project"]["name"]
         value["workspace"]  = value["workspace"]["name"]
         value["duration"] = ChronicDuration.output(value["duration"].to_i, :format => :short)
-        value["start"] = DateTime.parse(value["start"]).strftime("%d/%m/%Y")
+        value["start"] = value["start"].strftime("%d/%m/%Y")
       end
       values.view(:class => :table, :fields => TASK_FIELDS)
     end
@@ -39,7 +39,7 @@ module TogglCmd
     def self.prettify_projects(values)
       values.view(:class => :table, :fields => PROJECT_FIELDS)
     end
-            
+
   end
 
 end
